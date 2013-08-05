f43c8c29fb7521290a7304628931130fd29f15fd
John Kolovos <yannis.kolovos@gmail.com>
2013-08-05 22:44:02 +1000

    commend on initializer

--- lib/generators/stackreduce/templates/stackreduce_config.rb
+++ lib/generators/stackreduce/templates/stackreduce_config.rb
@@ -1,4 +1,9 @@
+# Be sure to restart your server when you modify this file.
 Stackreduce.configure do |config|
-  config.token = ""
-  config.app_id = ""
+  # Your applications id. Where you save the stacks. 
+  # This can be found under applications page
+  config.app_id = "YOU_APP_ID_HERE"
+  # Your applications secret token. 
+  # This can be found under applications page
+  config.token = "YOU_APP_TOKEN"
 end
