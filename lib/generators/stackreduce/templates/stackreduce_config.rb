9f1aa890ae58dc07937831317b4403e07e3e8d3b
John Kolovos <yannis.kolovos@gmail.com>
2013-08-08 00:09:02 +1000

    changed back to empty

--- lib/generators/stackreduce/templates/stackreduce_config.rb
+++ lib/generators/stackreduce/templates/stackreduce_config.rb
@@ -2,8 +2,8 @@
 Stackreduce.configure do |config|
   # Your applications id. Where you save the stacks. 
   # This can be found under applications page
-  config.app_id = "YOUR_APP_ID"
+  config.app_id = ""
   # Your applications secret token. 
   # This can be found under applications page
-  config.token = "YOUR_APP_TOKEN"
+  config.token = ""
 end
